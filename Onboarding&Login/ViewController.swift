74b5cd373a5652801350f6547f30b14c2c5380c5
Denis Goloborodko <goldena_by@outlook.com>
2021-09-23 17:28:57 +0300

    Initial Commit

---
+++ Onboarding&Login/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Onboarding&Login
+//
+//  Created by Denis Goloborodko on 23.09.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

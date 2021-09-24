e7222c8c52e8d988950384e255b8cef7abadaacc
Denis Goloborodko <goldena_by@outlook.com>
2021-09-24 18:36:08 +0300

    v 0.5 Setting up custom View

--- Onboarding&Login/Coordinators&ViewControllers/ViewControllers/SignupController.swift
+++ Onboarding&Login/Coordinators&ViewControllers/ViewControllers/SignupController.swift
@@ -8,54 +8,31 @@
 import UIKit
 
 final class SignupController: UIViewController, CoordinatableViewController {
-    
-    var coordinator: Coordinator!
-            
+                
     // MARK: - Properties
     
-    private lazy var label: UILabel = {
-        makeLabel(text: "Signup", font: .systemFont(ofSize: 32), addTo: view)
-    }()
-    
-    private lazy var button: UIButton = {
-        makeButton(type: .system, title: "to Login", font: .systemFont(ofSize: 20), addTo: view)
-    }()
+    var coordinator: Coordinator!
+
+    var signupView: SignupView {
+        view as! SignupView
+    }
     
     // MARK: - View Lifecycle
     
     override func loadView() {
-        let view = UIView()
-        view.backgroundColor = .gray
+        let signupView = SignupView()
 
-        self.view = view
+        self.view = signupView
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        #warning("Solve issue with frame and bounds being 0 at this point")
-        
-        self.setupViews()
-        self.setupTargets()
+                
+        setupTargets()
     }
-    
-    func setupViews() {
-        button.backgroundColor = .blue
         
-        button.layer.cornerRadius = button.frame.height / 2
-
-        label.centerXAnchor.constraint(equalTo: view.centerXAnchor).activate()
-        label.centerYAnchor.constraint(equalTo: view.centerYAnchor).activate()
-        
-        button.heightAnchor.constraint(equalToConstant: 50).activate()
-        button.widthAnchor.constraint(equalToConstant: 100).activate()
-        
-        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).activate()
-        button.centerYAnchor.constraint(equalTo: label.centerYAnchor, constant: 50).activate()
-    }
-    
     func setupTargets() {
-        button.addTarget(self, action: #selector(toLoginButtonPressed), for: .touchUpInside)
+        signupView.button.addTarget(self, action: #selector(toLoginButtonPressed), for: .touchUpInside)
     }
     
     // MARK: - Targets
--- Onboarding&Login/Views/SignupView.swift
+++ Onboarding&Login/Views/SignupView.swift
@@ -7,8 +7,61 @@
 
 import UIKit
 
-class SignupView: UIView {
+final class SignupView: UIView {
 
+    // MARK: - Properties
+        
+    private lazy var label: UILabel = {
+        makeLabel(text: "Signup", font: .systemFont(ofSize: 32), addTo: self)
+    }()
+    
+    lazy var button: UIButton = {
+        makeButton(type: .system, title: "to Login", font: .systemFont(ofSize: 20), addTo: self)
+    }()
 
+    // MARK: - View Lifecycle
+    
+    private func setupViews() {
+        backgroundColor = .gray
+        
+        button.backgroundColor = .blue
+        
+        #warning("Solve issue with frame and bounds being 0 at this point")
+        DispatchQueue.main.async {
+            self.button.layer.cornerRadius = self.button.frame.height / 2
+        }
+        
+        label.centerXAnchor.constraint(equalTo: centerXAnchor).activate()
+        label.centerYAnchor.constraint(equalTo: centerYAnchor).activate()
+        
+        button.heightAnchor.constraint(equalToConstant: 50).activate()
+        button.widthAnchor.constraint(equalToConstant: 100).activate()
+        
+        button.centerXAnchor.constraint(equalTo: centerXAnchor).activate()
+        button.centerYAnchor.constraint(equalTo: label.centerYAnchor, constant: 50).activate()
+    }
 
+    // MARK: - Controls
+    
+//    private func setupControls() {
+//        button.addTarget(self, action: #selector(buttonPressed), for: .touchUpInside)
+//    }
+//    
+//    @objc private func buttonPressed(_ sender: UIButton) {
+//        print("Button")
+//    }
+    
+    // MARK: - Init
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+        // setupControls()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
